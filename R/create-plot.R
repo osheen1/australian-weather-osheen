305f5f8a335afbcb2d63a8442106f5ab45e1e9c8
osheen1 <osheen@jumpingrivers.com>
2025-10-27 11:52:47 +0000

    sydney to perth

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,13 +9,14 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Sydney"
+city = "Perth"
 
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color = "red")
+  geom_point(color = "red") +
+  labs(title = "Title")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
