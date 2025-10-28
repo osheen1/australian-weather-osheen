f3ca9331a6ef4500125e5a3b46ec1033393088cc
osheen1 <osheen@jumpingrivers.com>
2025-10-28 13:09:30 +0000

    changing plot color to blue

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color = "red") +
+  geom_point(color = "blue") +
   labs(title = "Title") +
   theme_minimal()
 
